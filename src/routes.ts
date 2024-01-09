0e0ceb5ef5a57711bd234130cdf08abb1663c53f
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-01-09 17:49:32 -0300

    Added candidate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,16 @@
 import express, { Router } from "express";
 import { ApplicationDocumentsModule } from "./modules/ApplicationDocumentsModule";
+import { ApplicationStatusModule } from "./modules/ApplicationStatusModule";
+import { CandidateModule } from "./modules/CandidateModule";
+import { CompanyModule } from "./modules/CompanyModule";
+import { DashboardModule } from "./modules/DashboardModule";
+import { EmploymentHistoryModule } from "./modules/EmploymentHistoryModule";
+import { FeedbackModule } from "./modules/FeedbackModule";
+import { HiringProcessModule } from "./modules/HiringProcessModule";
+import { InterviewModule } from "./modules/InterviewModule";
+import { JobPostingModule } from "./modules/JobPostingModule";
+import { NotificationsModule } from "./modules/NotificationsModule";
+import { RecruiterModule } from "./modules/RecruiterModule";
 
 const router: Router = express.Router();
 router.use(express.json());
@@ -7,11 +18,28 @@ router.use(express.json());
 namespace ApplicationDocumentsRoutes {
   const { controller } = ApplicationDocumentsModule.make()
 
+  router.post("/applicationDocuments", controller.create.bind(controller));
+  router.get("/applicationDocuments/:id", controller.getById.bind(controller));
+  router.get("/applicationDocuments", controller.getAll.bind(controller));
+  router.patch("/applicationDocuments/:id", controller.update.bind(controller))
+  router.delete("applicationDocuments/:id", controller.delete.bind(controller))
+}
+
+namespace ApplicationStatusRoutes {
+  const { controller } = ApplicationStatusModule.make()
+
+  router.post("/")
+}
+
+namespace CandidateRoutes {
+  const { controller } = CandidateModule.make()
+
   router.post("/candidate", controller.create.bind(controller));
   router.get("/candidate/:id", controller.getById.bind(controller));
   router.get("/candidate", controller.getAll.bind(controller));
   router.patch("/candidate/:id", controller.update.bind(controller))
   router.delete("candidate/:id", controller.delete.bind(controller))
 }
+
 
 export { router }
